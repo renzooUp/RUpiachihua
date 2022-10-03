2b611a4a72ce7893e2fd38e610a8bbdb530ecc9f
renzooUp <renzoupiachihua3@outlook.com>
2022-10-03 17:37:30 -0500

    Subiendo mi examen del SpringBoot

---
+++ src/test/java/com/upia/RUpiachihua/RUpiachihuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upia.RUpiachihua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RUpiachihuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
